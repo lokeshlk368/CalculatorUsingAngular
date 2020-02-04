a618034c2fee68dfd20d61a9b2bd676174020f14
lokeshlk368 <lk689735@gmail.com>
2020-02-04 21:20:19 +0530

    files added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculatorUsingAngular';
+
+  public num1:number;
+  public num2:number;
+  public result:number;
+
+  Addition()
+  {
+    this.result=this.num1+this.num2;
+  }
+  Subtraction()
+  {
+    this.result=this.num1-this.num2;
+  }
+  Multification()
+  {
+    this.result=this.num1*this.num2;
+  }
+  Division()
+  {
+    this.result=(this.num1)/(this.num2);
+  }
 }
